f5658a2e0e69adcd0f87729ab71b5002d1c5ed96
tomoo sasaabe <sasa.sakurajima@gmail.com>
2020-09-27 18:08:18 +0900

    削除に伴うチームメンバーへのメール機能実装

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -20,10 +20,18 @@ class AgendasController < ApplicationController
       render :new
     end
   end
-
+  # アジェンダとチームidを中間にして、メンバーが紐づいている。has manyのメソッドを使う。
+  # この中でeach doを使って実装する。
   def destroy
     @agenda = Agenda.find(params[:id])
     if current_user.id == @agenda.user_id || current_user.id == Team.find(@agenda.team_id).owner_id
+      # binding.irb
+      @contact = @agenda.team.members
+      # 下記のコメントアウトしたコードだとリーダーのteam_idはとれないし、煩雑。membersメソッドについてはteamモデルのアソシエーションを参照。
+      # @contact = User.where(keep_team_id: @agenda.team_id)
+      @contact.each do |contact|
+      ContactMailer.contact_mail(contact).deliver
+      end
       @agenda.destroy
       redirect_to dashboard_path, notice:"削除しました"
     else
